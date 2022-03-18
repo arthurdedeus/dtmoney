98cd03635b26f2fc3a9d66f477de272c704a3473
Arthur Moreira de Deus <arthur.md@parade.ai>
2022-03-18 11:16:13 -0300

    feat: Set active color to buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -29,12 +29,12 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <input type="number" placeholder="Valor" />
 
         <TransactionTypeContainer>
-          <RadioBox type="button" onClick={() => setType('deposit')} isActive={type === 'deposit'}>
+          <RadioBox type="button" onClick={() => setType('deposit')} isActive={type === 'deposit'} activeColor="green">
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
           </RadioBox>
 
-          <RadioBox type="button" onClick={() => setType('withdraw')} isActive={type === 'withdraw'}>
+          <RadioBox type="button" onClick={() => setType('withdraw')} isActive={type === 'withdraw'} activeColor="red">
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
           </RadioBox>
