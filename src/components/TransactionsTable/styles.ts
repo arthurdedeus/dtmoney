4e693367e8a44b032d53e6127f0fa786c5fc3449
Arthur Moreira de Deus <54866778+arthurdedeus@users.noreply.github.com>
2022-03-23 19:43:50 -0300

    feat: Modal and forms (#4)
    * feat: Create transaction modal
    
    * feat: Create modal component
    
    * feat: Create form structure
    
    * feat: Style modal
    
    * feat: Add type buttons
    
    * feat: Make buttons selectable
    
    * feat: Set active color to buttons
    
    * feat: Save form data
    
    * feat: Post new transaction to API
    
    * feat: List transactions and seeds
    
    * feat: Format date and amount

--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -10,6 +10,7 @@ export const Container = styled.div`
     th {
       color: var(--text-body);
       font-weight: 400;
+      padding: 1rem 2rem;
       text-align: left;
       line-height: 1.5rem;
     }
@@ -20,6 +21,7 @@ export const Container = styled.div`
       background: var(--shape);
       color: var(--text-body);
       border-radius: 0.25rem;
+
 
       &:first-child {
         color: var(--text-title);
