ceea40fbd7050b34ea9558d3b30ef9e2f63e7c79
Arthur Moreira de Deus <arthur.md@parade.ai>
2022-03-24 10:16:24 -0300

    feat: Populating context with transactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,23 +1,10 @@
-import { useEffect, useState } from 'react'
-import { api } from '../../services/api'
+import { useContext} from 'react'
+import { TransactionsContext } from '../../TransactionsContext'
 import { Container } from './styles'
 
-interface Transaction {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  created_at: string;
-}
 
 export function TransactionsTable() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  useEffect(() => {
-    api.get('transactions')
-      .then(response => setTransactions(response.data.transactions))
-  }, [])
+  const transactions = useContext(TransactionsContext)
 
   return (
     <Container>
@@ -31,19 +18,25 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
+
           {transactions.map(transaction => (
             <tr key={transaction.id}>
+
               <td>{transaction.title}</td>
+
               <td className={transaction.type}>
                 {new Intl.NumberFormat('pt-BR', {
                   style: 'currency',
                   currency: 'BRL',
                 }).format(transaction.amount)}
               </td>
+
               <td>{transaction.category}</td>
+
               <td>
                 {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.created_at))}
               </td>
+
             </tr>
           ))}
 
