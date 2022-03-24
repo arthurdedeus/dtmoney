4b2c4509b7cd8ddc566338fb8f1f3597b1b1f713
Arthur Moreira de Deus <54866778+arthurdedeus@users.noreply.github.com>
2022-03-24 12:09:12 -0300

    Release/v1.0.0 (#7)
    * feat: Components (#2)
    
    * feat: Application structure (#1)
    
    * feat: Import assets
    
    * feat: Add styled components
    
    * feat: Add global styles
    
    * feat: Add custom fonts
    
    * feat: Create header
    
    * feat: Create dashboard
    
    * feat: Create transactions table
    
    * feat: Consuming from the api (#3)
    
    * feat: Configure miragejs
    
    * feat: Implement axios
    
    * feat: Modal and forms (#4)
    
    * feat: Create transaction modal
    
    * feat: Create modal component
    
    * feat: Create form structure
    
    * feat: Style modal
    
    * feat: Add type buttons
    
    * feat: Make buttons selectable
    
    * feat: Set active color to buttons
    
    * feat: Save form data
    
    * feat: Post new transaction to API
    
    * feat: List transactions and seeds
    
    * feat: Format date and amount
    
    * feat: Contexts and hooks (#5)
    
    * feat: Create TransactionsContext
    
    * feat: Populating context with transactions
    
    * feat: Move transaction creation to context
    
    * feat: Finish transaction input
    
    * feat: Calculate summary
    
    * feat: Create custom hook
    
    * docs: Update changelog file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import { GlobalStyle } from "./styles/globals";
+import { useState } from 'react'
+import Modal from 'react-modal'
+import { Dashboard } from './components/Dashboard'
+import { Header } from './components/Header'
+import { NewTransactionModal } from './components/NewTransactionModal'
+import { TransactionsProvider } from './hooks/useTransactions'
+import { GlobalStyle } from './styles/globals'
+
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
-    </div>
-  );
+    </TransactionsProvider>
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
+
+createServer({
+
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: '6000',
+          createdAt: new Date('2021-02-12'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: '1100',
+          createdAt: new Date('2021-02-14'),
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () =>{
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) =>{
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', {
+        ...data,
+        createdAt: new Date(),
+      })
+    })
+
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
